659820b9aceb53205ca528bf49da37ca286b4bb6
Jay Patel <104886785+jayPatel029@users.noreply.github.com>
2025-06-16 12:11:29 +0530

    admin login fix

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,12 +2,9 @@ const express = require("express");
 const router = express.Router();
 // const frontDescController = require('../controllers/frontDeskController');
 const authController = require('../controllers/authController');
-const { authenticate, authorizeRoles } = require("../middleware/auth");
+const { authenticate} = require("../middleware/auth");
 
 router.post("/login", authController.loginUserByRole);
-
-//! routes below this requires auth!!!
-router.use(authenticate);
 
 
 module.exports = router;
